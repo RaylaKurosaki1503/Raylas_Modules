4bf605342733165907bcae0c785ad251de3bc4ee
Rayla_Kurosaki <60267154+RaylaKurosaki1503@users.noreply.github.com>
2022-02-06 14:02:55 -0500

    Create excel.py

---
+++ rayla/excel.py
@@ -0,0 +1,12 @@
+################################################################################
+"""
+Author: Rayla
+Description: This file contains all easy-to-read functions in order to access
+             and edit Excel Workbooks.
+"""
+################################################################################
+
+import openpyxl
+
+if __name__ == '__main__':
+    pass
