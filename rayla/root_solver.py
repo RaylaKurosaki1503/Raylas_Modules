d18fe8b72d4332590095c03c16e733ade150f745
Rayla_Kurosaki <60267154+RaylaKurosaki1503@users.noreply.github.com>
2022-02-09 22:57:38 -0500

    Create root_solver.py

---
+++ rayla/root_solver.py
@@ -0,0 +1,8 @@
+"""
+Author: Rayla Kurosaki
+File: root_solver.py
+Description: This file contains functions to find the roots to linear and
+             nonlinear equations.
+"""
+
+################################################################################
