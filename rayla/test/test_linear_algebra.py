e5dfb77a98ee50397b47003e88753e53378f3563
Rayla_Kurosaki <60267154+RaylaKurosaki1503@users.noreply.github.com>
2022-02-13 20:04:54 -0500

    Create test_linear_algebra.py

---
+++ rayla/test/test_linear_algebra.py
@@ -0,0 +1,169 @@
+"""
+Author: Rayla Kurosaki
+File: test_linear_algebra.py
+Description: This file tests all the functions in linear_algebra.py.
+"""
+
+
+def test_dotP():
+    return
+
+
+def test_norm():
+    return
+
+
+def test_normalize():
+    return
+
+
+def test_distance():
+    return
+
+
+def test_angle_between_vectors():
+    return
+
+
+def test_is_orthogonal():
+    return
+
+
+def test_proj():
+    return
+
+
+def test_ref():
+    return
+
+
+def test_rank():
+    return
+
+
+def test_rref():
+    return
+
+
+def test_is_consistent():
+    return
+
+
+def test_is_linear_combination_vector():
+    return
+
+
+def test_is_linearly_independent_vector():
+    return
+
+
+def test_multiply():
+    return
+
+
+def test_power():
+    return
+
+
+def test_is_equal():
+    return
+
+
+def test_is_symmetric():
+    return
+
+
+def test_is_linear_combination_matrix():
+    return
+
+
+def test_is_linearly_independent_matrix():
+    return
+
+
+def test_is_square():
+    return
+
+
+def test_det():
+    return
+
+
+def test_gauss_jordan():
+    return
+
+
+def test_inverse_gj():
+    return
+
+
+def test_LU():
+    return
+
+
+def test_pLU():
+    return
+
+
+def test_is_row_space():
+    return
+
+
+def test_is_col_space():
+    return
+
+
+def test_basis_row_space():
+    return
+
+
+def test_basis_col_space():
+    return
+
+
+def test_basis_null_space():
+    return
+
+
+def test_dim():
+    return
+
+
+def test_nullity():
+    return
+
+
+if __name__ == '__main__':
+    test_dotP()
+    test_norm()
+    test_normalize()
+    test_distance()
+    test_angle_between_vectors()
+    test_is_orthogonal()
+    test_proj()
+    test_ref()
+    test_rank()
+    test_rref()
+    test_is_consistent()
+    test_is_linear_combination_vector()
+    test_is_linearly_independent_vector()
+    test_multiply()
+    test_power()
+    test_is_equal()
+    test_is_symmetric()
+    test_is_linear_combination_matrix()
+    test_is_linearly_independent_matrix()
+    test_is_square()
+    test_det()
+    test_gauss_jordan()
+    test_inverse_gj()
+    test_LU()
+    test_pLU()
+    test_is_row_space()
+    test_is_col_space()
+    test_basis_row_space()
+    test_basis_col_space()
+    test_basis_null_space()
+    test_dim()
+    test_nullity()
+    pass
