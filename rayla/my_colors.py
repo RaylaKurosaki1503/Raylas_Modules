10773d57388654a4a54d8fae7e25f7aaf57048d3
Rayla_Kurosaki <60267154+RaylaKurosaki1503@users.noreply.github.com>
2022-02-28 07:29:42 -0500

    Create my_colors.py

---
+++ rayla/my_colors.py
@@ -0,0 +1,34 @@
+"""
+Shades of Cyan
+
+Aqua = "#00FFFF"
+Celeste = "#B2FFFF"
+Electric Blue = "#7DF9FF"
+Sky Blue = "#80DAEB"
+
+Shades of Pink
+xxx = "#"
+xxx = "#"
+xxx = "#"
+xxx = "#"
+xxx = "#"
+xxx = "#"
+xxx = "#"
+xxx = "#"
+xxx = "#"
+xxx = "#"
+xxx = "#"
+xxx = "#"
+xxx = "#"
+xxx = "#"
+xxx = "#"
+xxx = "#"
+xxx = "#"
+xxx = "#"
+xxx = "#"
+xxx = "#"
+"""
+
+
+def get_math_colors():
+    return []
